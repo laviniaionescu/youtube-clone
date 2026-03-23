cfdd7ca2da6e98723f783ae8cff69fb59d1f915c
LaviniaIonescu <laviniaionescu9933@gmail.com>
2026-03-23 12:16:18 +0200

    added mobile menu dark overlay, close mobile menu functionality by clicking button and clicking outside of the menu

--- index.js
+++ index.js
@@ -17,11 +17,28 @@ burgerBtn.addEventListener("click", menuSwitch);
 
 function openMobileMenu() {
   const mobileMenu = document.querySelector(".mobile-menu")
+  const menuOverlay = document.querySelector(".menu-overlay")
 
   if (window.innerWidth <= 768) {
     mobileMenu.classList.add("active")
+    menuOverlay.classList.remove("is-hidden")
+    document.body.style.overflow = "hidden"
   }
 }
 
 const mobileMenuBtn = document.querySelector(".mobile-menu-button")
 mobileMenuBtn.addEventListener("click", openMobileMenu);
+
+function closeMobileMenu() {
+  const mobileMenu = document.querySelector(".mobile-menu")
+  const menuOverlay = document.querySelector(".menu-overlay")
+  document.body.style.overflow = ""
+
+  mobileMenu.classList.remove("active")
+  menuOverlay.classList.add("is-hidden")
+}
+
+const closeMobileMenuBtn = document.querySelector(".mobile-menu-close-btn")
+closeMobileMenuBtn.addEventListener('click', closeMobileMenu)
+const menuOverlay = document.querySelector(".menu-overlay")
+menuOverlay.addEventListener("click", closeMobileMenu)
